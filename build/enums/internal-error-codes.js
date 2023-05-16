43e29c1ac92e29db478cb14f7e11bb32b2112c6b
HoppeDevz <gabrielh2c@hotmail.com>
2023-05-16 09:25:15 -0300

    added build folder to gitignore

--- build/enums/internal-error-codes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.internalErrorCodes = void 0;
-exports.internalErrorCodes = {
-    InternalServerError: "ISE",
-    UserAlreadyExists: "UAE"
-};
